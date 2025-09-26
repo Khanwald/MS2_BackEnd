d48df5f0435d379a5ffca9a55731fdbeda411133
Khanwald <cso5@njit.edu>
2025-09-26 19:21:44 -0400

    Search Logic
    Incorporated the search logic to the members api and film api

--- app.py
+++ app.py
@@ -37,12 +37,26 @@ def films():
 @app.route('/api/members', methods=['GET'])
 def member():
     page = request.args.get('page', 1, type=int)
+    search = request.args.get('search', '', type=str)
+    value = request.args.get('value', '', type=str)
     items = 10
     index = (page - 1) * items
     end = page * items
     
+    
     result = "select customer_id, store_id, first_name, last_name from customer"
-    output = getQuery(result)
+    cond={}
+    if value == 'fname':
+        result = "select customer_id, store_id, first_name, last_name from customer where first_name like :search"
+        cond = {"search" : f"{search}%"}
+    elif value == 'lname':
+        result = "select customer_id, store_id, first_name, last_name from customer where last_name like :search"
+        cond = {"search" : f"{search}%"}
+    elif value == 'id':
+        result = "select customer_id, store_id, first_name, last_name from customer where customer_id like :search"
+        cond = {"search" : f"{search}%"}
+        
+    output = getQuery(result, cond)
     pages = output[index:end]
     totalPages= math.ceil(len(output)/items)
     
@@ -61,6 +75,25 @@ def profile(customer_id):
     output = getQuery(result, cond)
     return jsonify({"rental": output})
 
+@app.route('/api/getMovie', methods=['GET'])
+def getMovie():
+    title = request.args.get('title', '', type=str)
+    type = request.args.get('type', '', type=str)
+    
+    if type == "Title":
+        result = "select * from film where title LIKE :film_name"
+        cond = {"film_name" : f"{title}%"}
+    elif type == "Actor":
+        result = "select distinct f.film_id, f.title, CONCAT(a.first_name, ' ',a.last_name) as name from film_actor fa join actor a on fa.actor_id = a.actor_id join film f on f.film_id = fa.film_id where CONCAT(a.first_name, ' ', a.last_name) LIKE :actor_name order by f.film_id"
+        cond = {"actor_name" : f"{title}%"}
+    elif type == "Genre":
+        result="select f.film_id, f.title, fc.category_id, c.name from film f join film_category fc on f.film_id = fc.film_id join category c on fc.category_id = c.category_id where c.name like :genre"
+        cond = {"genre" : f"{title}%"}
+    else:
+        return jsonify({"movies": []})
+    
+    output = getQuery(result, cond)
+    return jsonify({"movies": output})
 
 if __name__ == "__main__":
     app.run(debug=True)
