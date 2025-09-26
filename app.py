de64795fbf8df8bb4bf5748625eeedac04e5346f
Khanwald <cso5@njit.edu>
2025-09-25 21:53:43 -0400

    Pagination Api
    I added pagination logic for customer table

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
-from flask import jsonify
+from flask import request, jsonify
 import config
+import math
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}"
 
 db = SQLAlchemy(app)
 
 
-@app.route('/films')
+@app.route('/api/films')
 def films():
     result = db.session.execute(text("select f.film_id, f.title, fc.category_id, c.name from film f join film_category fc on f.film_id = fc.film_id  join category c on fc.category_id = c.category_id"))
     output = []
@@ -24,9 +25,32 @@ def films():
     
     return jsonify({"films": output})
 
-@app.route('/members')
+@app.route('/api/members')
 def member():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    page = request.args.get('page', 1, type=int)
+    items = 10
+    index = (page - 1) * items
+    end = page * items
+    
+    result = db.session.execute(text("select customer_id, store_id, first_name, last_name from customer"))
+    output = []
+    for row in result:
+        output.append({
+            "customer_id": row.customer_id,
+            "store_id": row.store_id,
+            "first_name": row.first_name,
+            "last_name": row.last_name
+        })
+    pages = output[index:end]
+    totalPages= math.ceil(len(output)/items)
+    
+    return jsonify({
+        "page": page,
+        "customerNum": items,
+        "total": len(output),
+        "customers": pages,
+        "totalPages": totalPages
+    })
     
 if __name__ == "__main__":
     app.run(debug=True)
